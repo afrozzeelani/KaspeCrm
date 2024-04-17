4d9ab7c97599528828439c9856db4a69df46cfb6
Zeelani <65568348+afrozzeelani@users.noreply.github.com>
2024-04-17 17:20:43 +0530

    Update employeeRoute.js

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const employeeRoute = express.Router();
 
-const {
-  verifyAdminHR,
-  verifyEmployee
-} = require("../middleware/authMiddleware");
+// const {
+//   verifyAdminHR,
+//   verifyEmployee
+// } = require("../middleware/authMiddleware");
 
 const {
   getAllEmployee,
@@ -20,8 +20,8 @@ const {
 const { fileUploadMiddleware, checkFileSize } = require("../middleware/multer");
 
 // GET: Retrieve all countries
-employeeRoute.get("/employee/", verifyAdminHR, getAllEmployee);
-employeeRoute.get("/employee/:id?", verifyEmployee, getAllEmployee);
+employeeRoute.get("/employee/", getAllEmployee);
+employeeRoute.get("/employee/:id?", getAllEmployee);
 employeeRoute.get("/particularEmployee/:id", findParticularEmployee);
 employeeRoute.post("/notificationStatusUpdate/:id", notificationStatusUpdate);
 employeeRoute.post("/notificationDeleteHandler/:id", deleteNotification);
@@ -34,7 +34,7 @@ employeeRoute.post("/selectedNotificationDelete", selectedDeleteNotification);
 // verifyAdminHR
 employeeRoute.post(
   "/employee",
-  verifyAdminHR,
+
   fileUploadMiddleware,
   checkFileSize,
   createEmployee
@@ -43,7 +43,7 @@ employeeRoute.post(
 // PUT: Update an existing employee
 employeeRoute.put(
   "/employee/:id",
-  verifyAdminHR,
+
   fileUploadMiddleware,
   checkFileSize,
   updateEmployee
@@ -53,3 +53,61 @@ employeeRoute.put(
 // employeeRoute.delete("/employee/:id", verifyAdminHR, deleteEmployee);
 
 module.exports = employeeRoute;
+
+
+
+// const express = require("express");
+// const employeeRoute = express.Router();
+
+// const {
+//   verifyAdminHR,
+//   verifyEmployee
+// } = require("../middleware/authMiddleware");
+
+// const {
+//   getAllEmployee,
+//   createEmployee,
+//   updateEmployee,
+//   findParticularEmployee,
+//   selectedDeleteNotification,
+//   deleteNotification,
+//   notificationStatusUpdate,
+//   multiSelectedDeleteNotification,
+//   deleteEmployee
+// } = require("../controllers/employeeController");
+// const { fileUploadMiddleware, checkFileSize } = require("../middleware/multer");
+
+// // GET: Retrieve all countries
+// employeeRoute.get("/employee/", verifyAdminHR, getAllEmployee);
+// employeeRoute.get("/employee/:id?", verifyEmployee, getAllEmployee);
+// employeeRoute.get("/particularEmployee/:id", findParticularEmployee);
+// employeeRoute.post("/notificationStatusUpdate/:id", notificationStatusUpdate);
+// employeeRoute.post("/notificationDeleteHandler/:id", deleteNotification);
+// employeeRoute.post(
+//   "/multiSelectedNotificationDelete",
+//   multiSelectedDeleteNotification
+// );
+// employeeRoute.post("/selectedNotificationDelete", selectedDeleteNotification);
+// // POST: Create a new employee
+// // verifyAdminHR
+// employeeRoute.post(
+//   "/employee",
+//   verifyAdminHR,
+//   fileUploadMiddleware,
+//   checkFileSize,
+//   createEmployee
+// );
+
+// // PUT: Update an existing employee
+// employeeRoute.put(
+//   "/employee/:id",
+//   verifyAdminHR,
+//   fileUploadMiddleware,
+//   checkFileSize,
+//   updateEmployee
+// );
+
+// // DELETE: Delete a employee
+// // employeeRoute.delete("/employee/:id", verifyAdminHR, deleteEmployee);
+
+// module.exports = employeeRoute;
